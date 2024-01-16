e3f9d53677a34ff8c249223f02b157c5077e4ece
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-16 21:56:42 +0800

    谭浩强《C程序设计（第五版）》第四章---测试代码---4.1

---
+++ test_4/test_4/test.c
@@ -0,0 +1,130 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main(){
+//	double a,b,c,pb,x1,x2,a1,b1;
+//	printf("请输入a,b,c(用逗号隔开):");
+//	scanf("%lf,%lf,%lf",&a,&b,&c);
+//	pb = b*b-4*a*c;
+//	if(pb >= 0){
+//		if(pb > 0){
+//			x1 = (-b+sqrt(pb))/(2*a);
+//			x2 = (-b-sqrt(pb))/(2*a);
+//		}
+//		else{
+//			x1 = x2 = -b/(2*a);
+//		}
+//		printf("方程的根为：\nx1=%.2lf,x2=%.2lf\n",x1,x2);
+//	}
+//	else{
+//		a1 = -b/(2*a);
+//		b1 = sqrt(-pb)/(2*a);
+//		printf("方程的根为：\nx1=%.2lf+%.2lfi,x2=%.2lf-%.2lfi",a1,b1,a1,b1);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

20aeb59d16e1da1d6724a59ae1acb736472c0b64
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-17 09:16:56 +0800

    谭浩强《C程序设计（第五版）》第四章---测试代码---例4.3

--- test_4/test_4/test.c
+++ test_4/test_4/test.c
@@ -30,23 +30,119 @@
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	int num;
+//	printf("请输入一个整数：");
+//	scanf("%d",&num);
+//	if(num >= 0){
+//		num = num;
+//	}
+//	else{
+//		num = -num;
+//	}
+//	printf("绝对值为:%d\n",num);
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	double a,b;
+//	printf("请输入两个实数（用空格隔开）：");
+//	scanf("%lf%lf",&a,&b);
+//	if(a >= b){
+//		printf("从小到大：%.2lf,%.2lf\n",b,a);
+//	}
+//	else{
+//		printf("从小到大：%.2lf,%.2lf\n",a,b);
+//	}
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	double a,b,c;
+//	printf("请输入三个实数（用空格隔开）：");
+//	scanf("%lf%lf%lf",&a,&b,&c);
+//	if(a >= b){
+//		if(b >= c){
+//			printf("从小到大：%.2lf,%.2lf,%.2lf\n",c,b,a);
+//		}
+//		else{
+//			if(a >= c){
+//				printf("从小到大：%.2lf,%.2lf,%.2lf\n",b,c,a);
+//			}
+//			else{
+//				printf("从小到大：%.2lf,%.2lf,%.2lf\n",b,a,c);
+//			}
+//		}
+//	}
+//	else{
+//		if(a >= c){
+//			printf("从小到大：%.2lf,%.2lf,%.2lf\n",c,a,b);
+//		}
+//		else{
+//			if(b >= c){
+//				printf("从小到大：%.2lf,%.2lf,%.2lf\n",a,c,b);
+//			}
+//			else{
+//				printf("从小到大：%.2lf,%.2lf,%.2lf\n",a,b,c);
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	double a,b,t;
+//	printf("请输入两个实数（用空格隔开）：");
+//	scanf("%lf%lf",&a,&b);
+//	if(a >= b){
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	printf("从小到大：%.2lf,%.2lf\n",a,b);
+//	return 0;
+//}
 
 
 
-
-
-
-
-
+//#include <stdio.h>
+//
+//int main(){
+//	double a,b,c,t;
+//	printf("请输入三个实数（用空格隔开）：");
+//	scanf("%lf%lf%lf",&a,&b,&c);
+//	if(a >= b){
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if(a >= c){
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if(b >= c){
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("从小到大：%.2lf,%.2lf,%.2lf\n",a,b,c);
+//	return 0;
+//}
 
 
 

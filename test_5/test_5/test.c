477dfc877860ed3ffd1e48179529597fb8b5fd6f
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-17 21:01:22 +0800

    谭浩强《C程序设计（第五版）》第四章---测试代码---例5.1

---
+++ test_5/test_5/test.c
@@ -0,0 +1,128 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int i=1,sum=0;
+//	while(i <= 100){
+//		sum = sum + i;
+//		i++;
+//	}
+//	printf("sum = %d\n",sum);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int i=1,sum=0;
+//	do{
+//		sum = sum + i;
+//		i++;
+//	}while(i <= 100);
+//	printf("sum = %d\n",sum);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

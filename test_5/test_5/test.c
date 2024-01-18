a2be7333b8843c5cda9d38876e830447739b29bd
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-18 17:42:54 +0800

    谭浩强《C程序设计（第五版）》第四章---测试代码---习8

--- test_5/test_5/test.c
+++ test_5/test_5/test.c
@@ -111,40 +111,181 @@
 
 
 
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main(){
+//	int n,i=2;
+//	printf("请输入大于3的整数：");
+//	scanf("%d",&n);
+//	while(i <= sqrt((double)n)){
+//		if(n%i==0)break;
+//		i++;
+//	}
+//	if(i <= sqrt((double)n)){
+//		printf("%d不是素数\n",n);
+//	}
+//	else{
+//		printf("%d是素数\n",n);
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main(){
+//	int i=100,n=2;
+//	while(i <= 200){
+//		while(n <= sqrt((double)i)){
+//			if(i%n==0)break;
+//			n++;
+//		}
+//		if(n > sqrt((double)i))printf("%d\n",i);
+//		n = 2;
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	char c;
+//	c = getchar();
+//	while(c!='\n'){
+//		if((c>='A'&&c<='Z')||(c>='a'&&c<='z')){
+//			if((c>='W'&&c<='Z')||(c>='w'&&c<='z')){
+//				c = c - 22;
+//			}
+//			c = c + 4;
+//		}
+//		putchar(c);
+//		c = getchar();
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int m,n,r,p;
+//	printf("请输入两个整数（用空格隔开）：");
+//	scanf("%d%d",&m,&n);
+//	p = m*n;
+//	if(m < n){
+//		r = m;
+//		m = n;
+//		n = r;
+//	}
+//	r = m % n;
+//	while(r != 0){
+//		m = n;
+//		n = r;
+//		r = m % n;
+//	}
+//	printf("最大公约数=%d\t最小公倍数=%d\n",n,p/n);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	char c;
+//	int ec=0,kc=0,sc=0,qc=0;
+//	printf("请输入字符串（以回车结束）：");
+//	while((c=getchar()) != '\n'){
+//		if((c>='A'&&c<='Z')||(c>='a'&&c<='z')){
+//			ec++;
+//		}
+//		else if(c==' '){
+//			kc++;
+//		}
+//		else if(c>='0'&&c<='9'){
+//			sc++;
+//		}
+//		else{
+//			qc++;
+//		}
+//	}
+//	printf("英文字母：%d个\n空格：%d个\n数字：%d个\n其他字符：%d个\n",ec,kc,sc,qc);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int a,n,i,sum=0,mx=0;
+//	printf("请输入a,n（用空格隔开）：");
+//	scanf("%d%d",&a,&n);
+//	for(i=1;i<=n;i++){
+//		mx = mx*10+a;
+//		sum = sum +mx;
+//	}
+//	printf("sum = %d\n",sum);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int i,j;
+//	double sum = 0,mx = 1;
+//	for(i=1;i<=20;i++){
+//		for(j=1;j<=i;j++){
+//			mx = mx*j;
+//		}
+//		sum = sum + mx;
+//		mx = 1;
+//	}
+//	printf("sum = %22.15e\n",sum);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	double x1=0,x2=0,x3=0;
+//	int i;
+//	for(i=1;i<=100;i++){
+//		x1 = x1 + i;
+//	}
+//	for(i=1;i<=50;i++){
+//		x2 = x2 + i*i;
+//	}
+//	for(i=1;i<=10;i++){
+//		x3 = x3 + 1.0/i;
+//	}
+//	printf("%.6lf\n",x1+x2+x3);
+//	return 0;
+//}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//#include <stdio.h>
+//
+//int main(){
+//	int i;
+//	for(i=100;i<=999;i++){
+//		if(i == ((i/100)*(i/100)*(i/100)+(i%100/10)*(i%100/10)*(i%100/10)+(i%10)*(i%10)*(i%10))) printf("%d\n",i);
+//	}
+//	return 0;
+//}
 
 
 

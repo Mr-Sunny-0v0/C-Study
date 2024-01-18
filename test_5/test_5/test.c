6446a95fb7d5bd78db4e3bd93be6eb5b09861a03
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-18 11:36:08 +0800

    谭浩强《C程序设计（第五版）》第四章---测试代码---例5.8

--- test_5/test_5/test.c
+++ test_5/test_5/test.c
@@ -5,7 +5,7 @@
 //#include <stdio.h>
 //
 //int main(){
-//	int i=1,sum=0;
+//	int i = 1,sum = 0;
 //	while(i <= 100){
 //		sum = sum + i;
 //		i++;
@@ -19,7 +19,7 @@
 //#include <stdio.h>
 //
 //int main(){
-//	int i=1,sum=0;
+//	int i = 1,sum = 0;
 //	do{
 //		sum = sum + i;
 //		i++;
@@ -30,33 +30,84 @@
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	int i;
+//	double sum=0.0,m;
+//	for(i=1;i<=1000;i++){
+//		printf("请输入捐款金额：");
+//		scanf("%lf",&m);
+//		sum = sum + m;
+//		if(sum >= 100000) break;
+//	}
+//	printf("捐款人数：%d\t平均每人捐款数目：%lf\n",i,sum/i);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int i;
+//	for(i=100;i<=200;i++){
+//		if(i%3==0) continue;
+//		printf("%d\t",i);
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int i,j;
+//	for(i=1;i<=4;i++){
+//		for(j=1;j<=5;j++){
+//			printf("%d\t",i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main(){
+//	int sign = 1;
+//	double fm = 1.0,sum = 0.0,pi;
+//	while(fabs(1/fm) >= 1e-6){
+//		sum = sum + sign * (1 / fm);
+//		sign = (-1) * sign;
+//		fm = fm + 2.0;
+//	}
+//	pi = 4 * sum;
+//	printf("pi = %.8lf\n",pi);
+//	return 0;
+//}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//#include <stdio.h>
+//
+//int main(){
+//	int i=3,f1=1,f2=1,f3;
+//	printf("%d\n%d\n",f1,f2);
+//	while(i<=40){
+//		f3 = f1 + f2;
+//		printf("%d\n",f3);
+//		f1 = f2;
+//		f2 = f3;
+//		i++;
+//	}
+//	return 0;
+//}
 
 
 

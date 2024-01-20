ae24feb7254be70a78a4cb272c670700d4957ba8
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-20 21:25:49 +0800

    谭浩强《C程序设计（第五版）》第七章---测试代码---例7.4

---
+++ test_7/test_7/test.c
@@ -0,0 +1,180 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+//#include <stdio.h>
+//
+//void print_star(){
+//	printf("******************\n");
+//}
+//
+//void print_word(){
+//	printf("  How do you do!\n");
+//}
+//
+//int main(){
+//	print_star();
+//	print_word();
+//	print_star();
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int Max(int x,int y){
+//	if(x>y){
+//		return x;
+//	}
+//	else{
+//		return y;
+//	}
+//}
+//
+//int main(){
+//	int num1,num2,max;
+//	printf("请输入两个数（空格隔开）：");
+//	scanf("%d%d",&num1,&num2);
+//	max = Max(num1,num2);
+//	printf("最大值 = %d\n",max);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int Max(float x,float y){
+//	float z;
+//	if(x>y){
+//		z=x;
+//	}
+//	else{
+//		z=y;
+//	}
+//	return z;
+//}
+//
+//int main(){
+//	double num1,num2;
+//	int max;
+//	printf("请输入两个数（用空格隔开）：");
+//	scanf("%lf%lf",&num1,&num2);
+//	max = Max(num1,num2);
+//	printf("max = %d\n",max);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//double Sum(double x,double y){
+//	return x+y;
+//}
+//
+//int main(){
+//	double num1,num2,sum;
+//	printf("请输入两个实数（用空格隔开）：");
+//	scanf("%lf%lf",&num1,&num2);
+//	sum = Sum(num1,num2);
+//	printf("sum = %lf\n",sum);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

fa7c56214fb0ac904d67afcdd7c0cfc0bc305db0
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-23 17:37:15 +0800

    谭浩强《C程序设计（第五版）》第八章---测试代码---例8.3

---
+++ test_8/test_8/test.c
@@ -0,0 +1,166 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int a=100,b=10;
+//	int *pointer_1,*pointer_2;
+//	pointer_1 = &a;
+//	pointer_2 = &b;
+//	* pointer_1 = 5;
+//	printf("a = %d,b = %d\n",a,b);
+//	printf("*pointer_1 = %d,*pointer_2 = %d\n",*pointer_1,*pointer_2);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main(){
+//	int *p1,*p2,*p,a,b;
+//	printf("请输入两个数：");
+//	scanf("%d%d",&a,&b);
+//	p1 = &a;
+//	p2 = &b;
+//	if(a<b){
+//		p = p1;
+//		p1 = p2;
+//		p2 = p;
+//	}
+//	printf("a = %d,b = %d\n",a,b);
+//	printf("max = %d,min = %d\n",*p1,*p2);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//void swap(int *p1,int *p2){
+//	int t;
+//	t = *p1;
+//	*p1 = *p2;
+//	*p2 = t;
+//}
+//
+//int main(){
+//	int a,b;
+//	int * pointer_1,* pointer_2;
+//	printf("请输入两个数：");
+//	scanf("%d,%d",&a,&b);
+//	pointer_1 = &a;
+//	pointer_2 = &b;
+//	if(a<b)
+//		swap(pointer_1,pointer_2);
+//	printf("a = %d,b = %d\n",a,b);
+//	printf("max = %d,min = %d\n",*pointer_1,*pointer_2);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

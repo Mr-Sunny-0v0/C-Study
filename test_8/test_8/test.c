eb16cf887d367b6cc457e3690a182b5d393f4523
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-23 21:58:36 +0800

    谭浩强《C程序设计（第五版）》第八章---测试代码---例8.5

--- test_8/test_8/test.c
+++ test_8/test_8/test.c
@@ -22,7 +22,7 @@
 //int main(){
 //	int *p1,*p2,*p,a,b;
 //	printf("请输入两个数：");
-//	scanf("%d%d",&a,&b);
+//	scanf("%d,%d",&a,&b);
 //	p1 = &a;
 //	p2 = &b;
 //	if(a<b){
@@ -39,7 +39,7 @@
 
 //#include <stdio.h>
 //
-//void swap(int *p1,int *p2){
+//void swap(int *p1,int *p2){		//改变两个指针变量“所指变量”的值，可以实现
 //	int t;
 //	t = *p1;
 //	*p1 = *p2;
@@ -62,11 +62,63 @@
 
 
 
+//#include <stdio.h>
+//
+//void swap(int *p1,int *p2){		//改变两个指针变量的值，不能实现
+//	int *p;
+//	p = p1;
+//	p1 = p2;
+//	p2 = p;
+//}
+//
+//int main(){
+//	int a,b;
+//	int *pointer_1,*pointer_2;
+//	printf("请输入两个数：");
+//	scanf("%d,%d",&a,&b);
+//	pointer_1 = &a;
+//	pointer_2 = &b;
+//	if(a<b)
+//		swap(pointer_1,pointer_2);
+//	printf("a = %d,b = %d\n",a,b);
+//	printf("max = %d,min = %d\n",*pointer_1,*pointer_2);
+//	return 0;
+//}
 
 
 
-
-
+//#include <stdio.h>
+//
+//void sort(int *q1,int *q2,int *q3){		//改变指针变量“所指变量”的值，可以实现
+//	int t;
+//	if(*q1<*q2){
+//		t = *q1;
+//		*q1 = *q2;
+//		*q2 = t;
+//	}
+//	if(*q2<*q3){
+//		t = *q2;
+//		*q2 = *q3;
+//		*q3 = t;
+//	}
+//	if(*q1<*q2){
+//		t = *q1;
+//		*q1 = *q2;
+//		*q2 = t;
+//	}
+//}
+//
+//int main(){
+//	int a,b,c,*p1,*p2,*p3;
+//	printf("请输入三个数：");
+//	scanf("%d%d%d",&a,&b,&c);
+//	p1 = &a;
+//	p2 = &b;
+//	p3 = &c;
+//	sort(p1,p2,p3);
+//	printf("排序后：%d,%d,%d\n",*p1,*p2,*p3);
+//	return 0;
+//}
 
 
 

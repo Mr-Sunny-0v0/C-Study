1aa9429c515e0fefebee290f05d46bc899c532a8
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-01-25 11:36:01 +0800

    谭浩强《C程序设计（第五版）》第八章---测试代码---例8.19

--- test_8/test_8/test.c
+++ test_8/test_8/test.c
@@ -316,53 +316,191 @@
 
 
 
-#include <stdio.h>
-
-int main(){
-	int a[3][4] = {1,3,5,7,9,11,13,15,17,19,21,23};
-	printf("a---->%d\t\t*a---->%d\n",a,*a);
-	printf("a[0]---->%d\t*(a+0)---->%d\n",a[0],*(a+0));
-	printf("&a[0]---->%d\t&a[0][0]---->%d\n",&a[0],&a[0][0]);
-	printf("a[1]---->%d\ta+1---->%d\n",a[1],a+1);
-	printf("&a[1][0]---->%d\t*(a+1)+0---->%d\n",&a[1][0],*(a+1)+0);
-	printf("a[2]---->%d\t*(a+2)---->%d\n",a[2],*(a+2));
-	printf("&a[2]---->%d\ta+2---->%d\n",&a[2],a+2);
-	printf("a[1][0]---->%d\t\t*(*(a+1)+0)---->%d\n",a[1][0],*(*(a+1)+0));
-	printf("*a[2]---->%d\t\t*(*(a+2)+0)---->%d\n",*a[2],*(*(a+2)+0));
-	return 0;
-}
-
-
+//#include <stdio.h>
+//
+//int main(){
+//	int a[3][4] = {1,3,5,7,9,11,13,15,17,19,21,23};
+//	printf("a---->%d\t\t*a---->%d\n",a,*a);
+//	printf("a[0]---->%d\t*(a+0)---->%d\n",a[0],*(a+0));
+//	printf("&a[0]---->%d\t&a[0][0]---->%d\n",&a[0],&a[0][0]);
+//	printf("a[1]---->%d\ta+1---->%d\n",a[1],a+1);
+//	printf("&a[1][0]---->%d\t*(a+1)+0---->%d\n",&a[1][0],*(a+1)+0);
+//	printf("a[2]---->%d\t*(a+2)---->%d\n",a[2],*(a+2));
+//	printf("&a[2]---->%d\ta+2---->%d\n",&a[2],a+2);
+//	printf("a[1][0]---->%d\t\t*(*(a+1)+0)---->%d\n",a[1][0],*(*(a+1)+0));
+//	printf("*a[2]---->%d\t\t*(*(a+2)+0)---->%d\n",*a[2],*(*(a+2)+0));
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	int i,j,*p,a[3][4];
+//	printf("请输入12个整数：");
+//	for(i=0;i<=2;i++){
+//		for(j=0;j<=3;j++){
+//			scanf("%d",&a[i][j]);
+//			//scanf("%d",*(a+i)+j);
+//		}
+//	}
+//	for(p=a[0];p<=a[0]+11;p++){
+//		if((p-a[0])%4==0)
+//			printf("\n");
+//		printf("%4d",*p);
+//	}
+//	printf("\n");
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	int i,j,(*p)[4],a[3][4];
+//	p=a;
+//	printf("请输入12个整数：\n");
+//	for(i=0;i<=2;i++){
+//		for(j=0;j<=3;j++){
+//			scanf("%d",*(p+i)+j);
+//		}
+//	}
+//	p=a;
+//	printf("请输入行，列（用空格隔开）：");
+//	scanf("%d%d",&i,&j);
+//	printf("a[%d][%d] = %d\n",i-1,j-1,*(*(p+i-1)+j-1));
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//double aver(double (*p)[4],int n){
+//	int i,j;
+//	double sum=0.0;
+//	for(i=0;i<=2;i++){
+//		for(j=0;j<=3;j++){
+//			sum = sum + *(*(p+i)+j);
+//		}	
+//	}
+//	return sum/n;
+//}
+//
+//void search(double (*p)[4],int n){
+//	int i;
+//	printf("第%d个学生的成绩：",n);
+//	for(i=0;i<=3;i++){
+//		printf("%lf ",*(*(p+n-1)+i));
+//	}
+//	printf("\n");
+//}
+//
+//int main(){
+//	int i,j;
+//	double score[3][4];
+//	for(i=0;i<=2;i++){
+//		printf("请输入第%d个学生的成绩：",i+1);
+//		for(j=0;j<=3;j++){
+//			scanf("%lf",*(score+i)+j);
+//		}
+//	}
+//	printf("总平均分 = %lf\n",aver(score,12));
+//	search(score,2);
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//void search_bjg(double (*p)[4],int i,int j){
+//	int k;
+//	for(i=0;i<=2;i++){
+//		for(j=0;j<=3;j++){
+//			if(*(*(p+i)+j)<60){
+//				printf("第%d个学生不及格，成绩为：",i+1);
+//				for(k=0;k<=3;k++){
+//					printf("%lf ",*(*(p+i)+k));
+//				}
+//				printf("\n");
+//				break;
+//			}
+//		}
+//	}
+//}
+//
+//int main(){
+//	double score[3][4];
+//	int i,j;
+//	for(i=0;i<=2;i++){
+//		printf("请输入第%d个学生的成绩：",i+1);
+//		for(j=0;j<=3;j++){
+//			scanf("%lf",*(score+i)+j);
+//		}
+//	}
+//	search_bjg(score,3,4);
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	char s[100];
+//	printf("请输入字符串：");
+//	gets(s);
+//	printf("%s\n",s);
+//	printf("%c\n",s[7]);
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	char s[100];
+//	char *p=s;
+//	printf("请输入字符串：");
+//	gets(p);
+//	printf("%s\n",p);
+//	return 0;
+//}
 
 
 
+//#include <stdio.h>
+//
+//int main(){
+//	char s1[]="I am a student.",s2[20];
+//	int i;
+//	for(i=0;*(s1+i)!='\0';i++){
+//		*(s2+i) = *(s1+i);
+//	}
+//	*(s2+i) = '\0';
+//	printf("%s\n",s1);
+//	puts(s2);
+//	return 0;
+//}
 
 
 
-
-
-
-
-
-
+//#include <stdio.h>
+//
+//int main(){
+//	char s1[]="I am a atudent.",s2[20],*p1,*p2;
+//	for(p1=s1,p2=s2;*p1!='\0';p1++,p2++){
+//		*p2 = *p1;
+//	}
+//	*p2 = '\0';
+//	puts(s1);
+//	puts(s2);
+//	return 0;
+//}
 
 
 

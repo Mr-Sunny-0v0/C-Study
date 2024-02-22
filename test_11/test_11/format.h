a527adfcad314b1d574a259c0e6aaa533c1637df
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-02-22 22:18:01 +0800

    谭浩强《C程序设计（第五版）》第十一章---测试代码---例11.6

---
+++ test_11/test_11/format.h
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#define PR printf
+#define NL "\n"
+#define D "%d"
+#define D1 D NL
+#define D2 D D NL
+#define D3 D D D NL
+#define D4 D D D D NL
+#define S "%s" NL
---
+++ test_11/test_11/test.c
@@ -0,0 +1,273 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+//#include <stdio.h>
+//#define PI 3.1415926
+//
+//int main(){
+//	double l,s,v,r;
+//	printf("请输入圆的半径：");
+//	scanf("%lf",&r);
+//	l = 2 * PI * r;
+//	s = PI * r * r;
+//	v = 4.0/3 * PI * r * r * r;
+//	printf("圆周长 = %.4lf\n圆面积 = %.4lf\n圆球体积 = %.4lf\n",l,s,v);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#define R 3.0
+//#define PI 3.1415926
+//#define L 2*PI*R
+//#define S PI*R*R
+//
+//int main(){
+//	printf("L=%lf\nS=%lf\n",L,S);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#define PI 3.1415926
+//#define S(r) PI*r*r
+//
+//int main(){
+//	double a=3.6,area;
+//	area = S(a);
+//	printf("r = %lf\narea = %lf\n",a,area);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#define PI 3.1415926
+//#define CIRCLE(R,L,S,V) L=2*PI*R;S=PI*R*R;V=4.0/3*PI*R*R*R
+//
+//int main(){
+//	double r,l,s,v;
+//	printf("请输入半径r：");
+//	scanf("%lf",&r);
+//	CIRCLE(r,l,s,v);
+//	printf("r=%lf\nl=%lf\ns=%lf\nv=%lf\n",r,l,s,v);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#define PR printf
+//#define NL "\n"
+//#define D "%d"
+//#define D1 D NL
+//#define D2 D D NL
+//#define D3 D D D NL
+//#define D4 D D D D NL
+//#define S "%s\n"
+//
+//int main(){
+//	int a,b,c,d;
+//	char string[] = "CHINA";
+//	a=1;b=2;c=3;d=4;
+//	PR(D1,a);
+//	PR(D2,a,b);
+//	PR(D3,a,b,c);
+//	PR(D4,a,b,c,d);
+//	PR(S,string);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#include "format.h"
+//
+//int main(){
+//	int a,b,c,d;
+//	char string[] = "CHINA";
+//	a=1;b=2;c=3;d=4;
+//	PR(D1,a);
+//	PR(D2,a,b);
+//	PR(D3,a,b,c);
+//	PR(D4,a,b,c,d);
+//	PR(S,string);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
